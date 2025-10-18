8acf72e8bff86ac9a3d35b37515022f216f52546
Hifzan <hifzan.ahmed@gmail.com>
2025-10-19 01:06:43 +0200

    refactored

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from training.signature_training import SignatureTraining
 from verification.signature_verification import SignatureVerificationTraining 
 
 def main():
-    location_of_training_signature = 'data/saba/'
+    location_of_training_signature = 'data/SA/'
     size_of_training_signature = 6
-    location_of_test_signature = 'data/saba/signature7.png'
+    location_of_test_signature = 'data/SA/signature7.png'
     # Training Phase
-    s1 = SignatureTraining.training_genuine_with_siamese(location_of_training_signature, size_of_training_signature) 
+    print("🚀 Training Started...")
+    model = SignatureTraining.training_genuine_with_siamese(location_of_training_signature, size_of_training_signature) 
     # Verification Phase of input test signature 
-    s2 = SignatureVerificationTraining.verify_with_siamese_network(location_of_test_signature, s1 )
-    print("S1 (Training Score):", s1)
-    print("S2 (Verification Score):", s2)   
-    print(f"Similarity Score: {s2:.4f}")
-    
-    if s2 > 0.5:
+    print("\n🔍 Verifying Test Signature...")
+    similarity  = SignatureVerificationTraining.verify_test_signature_with_siamese_network(location_of_test_signature, model)
+    print(f"Similarity Score: {similarity :.4f}")
+     
+    if similarity > 0.5:
         print("✅ Likely Genuine")
     else:
         print("❌ Likely Forged")
